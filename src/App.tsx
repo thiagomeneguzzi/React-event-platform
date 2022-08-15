6f7728fc073563602180ec5252f049ba8db35c66
thiagomeneguzzi <thiagonmeneguzzi@gmail.com>
2022-08-15 10:58:40 -0300

    feat - implemented header and sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,8 @@
-import { gql, useQuery } from "@apollo/client";
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: string;
-  title: string;
-}
+import { Evento } from "./pages/Evento"
 
 function App() {
-
-  const { data } = useQuery(GET_LESSONS_QUERY)
-
   return (
-    <ul>
-      {
-        data.lessons.map((lesson: Lesson) => (
-          <li key={lesson.id}>{lesson.title}</li>
-        ))
-      }
-    </ul>
+    <Evento />
   )
 }
 
